d6da617ded65ae4658421a46eee864a05df79b36
Uday Damerla <135194953+UdayDamerla@users.noreply.github.com>
2026-09-23 14:41:54 +0530

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.java
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -1,27 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+
         if (s.length() != t.length()) {
-                    return false;
-                }
+            return false;
+        }
 
-                HashMap<Character, Integer> map = new HashMap<>();
-                
-                for (char c : s.toCharArray()) {
-                    map.put(c, map.getOrDefault(c, 0) + 1);
-                }
+        int[] count = new int[26];
 
-                for (char c : t.toCharArray()) {
-                    if (!map.containsKey(c)) {
-                        return false;
-                    }
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
 
-                    map.put(c, map.get(c) - 1);
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
 
-                    if (map.get(c) == 0) {
-                        map.remove(c);
-                    }
-                }
-
-                return map.isEmpty();
+        return true;
     }
 }
